1f4b00e0e7738d35843ffff7f84d6e8f9f97edda
Aviad Reich <avi.rei@gmail.com>
2011-01-03 22:21:44 +0200

    Logging added.

--- src/Stomp/Client.java
+++ src/Stomp/Client.java
@@ -2,14 +2,20 @@ package Stomp;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.io.*;
 import java.net.Socket;
 import javax.security.auth.login.LoginException;
 
 /**
- * Implements a Stomp client connection to a Stomp server via the network.  
- *
+ * Implements a Stomp client connection to a Stomp server via the network.
+ * 
  * Example:
+ * 
  * <pre>
  *   Client c = new Client( "localhost", 61626, "ser", "ser" );
  *   c.subscribe( "/my/channel", new Listener() { ... } );
@@ -18,77 +24,129 @@ import javax.security.auth.login.LoginException;
  *   // ...
  *   c.disconnect();
  * </pre>
- *
+ * 
  * @see Stomp
- *
- * (c)2005 Sean Russell
+ * 
+ *      (c)2005 Sean Russell
  */
 public class Client extends Stomp implements MessageReceiver {
-  private Thread _listener;
-  private OutputStream _output;
-  private InputStream _input;
-  private Socket _socket;
+	private Thread _listener;
+	private OutputStream _output;
+	private InputStream _input;
+	private Socket _socket;
+	public Logger _logger;
 
-  /**
-   * Connects to a server
-   *
-   * Example:
-   * <pre>
-   *   Client stomp_client = new Client( "host.com", 61626 );
-   *   stomp_client.subscribe( "/my/messages" );
-   *   ...
-   * </pre>
-   *
-   * @see Stomp
-   * @param server The IP or host name of the server
-   * @param port The port the server is listening on
-   */
-  public Client( String server, int port, String login, String pass )
-  throws IOException, LoginException {
-    _socket = new Socket( server, port );
-    _input = _socket.getInputStream();
-    _output = _socket.getOutputStream();
+	/**
+	 * Connects to a server
+	 * 
+	 * Example:
+	 * 
+	 * <pre>
+	 *   Client stomp_client = new Client( "host.com", 61626 );
+	 *   stomp_client.subscribe( "/my/messages" );
+	 *   ...
+	 * </pre>
+	 * 
+	 * @see Stomp
+	 * @param server
+	 *            The IP or host name of the server
+	 * @param port
+	 *            The port the server is listening on
+	 */
+	public Client(String server, int port, String login, String pass)
+			throws IOException, LoginException {
+		_socket = new Socket(server, port);
+		_input = _socket.getInputStream();
+		_output = _socket.getOutputStream();
 
-    _listener = new Receiver( this, _input );
-    _listener.start();
+		_listener = new Receiver(this, _input);
+		_listener.start();
 
-    // Connect to the server
-    HashMap header = new HashMap();
-    header.put( "login", login );
-    header.put( "passcode", pass );
-    transmit( Command.CONNECT, header, null );
-    try {
-      String error = null;
-      while (!isConnected() && ((error=nextError()) == null)) { 
-        Thread.sleep(100); 
-      }
-      if (error != null) throw new LoginException( error );
-    } catch (InterruptedException e) {
-    }
-  }
+		// Connect to the server
+		HashMap header = new HashMap();
+		header.put("login", login);
+		header.put("passcode", pass);
+		transmit(Command.CONNECT, header, null);
+		try {
+			String error = null;
+			while (!isConnected() && ((error = nextError()) == null)) {
+				Thread.sleep(100);
+			}
+			if (error != null)
+				throw new LoginException(error);
+		} catch (InterruptedException e) {
+		}
+		/* create the logger */
+		_logger = Logger.getLogger("SplAssignment3.Logger");
+		// Logger logger = new Logg ("SplAssignment1.Logger");
+		Handler fh = null;
+		try {
+			fh = new FileHandler(login + ".log");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		_logger.setUseParentHandlers(false);
 
-  public boolean isClosed() { return _socket.isClosed(); }
+		// logger output is written to a file in fh handler
+		fh.setFormatter(new OurFormatter());
+		_logger.addHandler(fh);
 
-  public void disconnect( Map header ) {
-    if (!isConnected()) return;
-    transmit( Command.DISCONNECT, header, null );
-    _listener.interrupt();
-    Thread.yield();
-    try { _input.close();  } catch (IOException e) {/* We ignore these. */}
-    try { _output.close(); } catch (IOException e) {/* We ignore these. */}
-    try { _socket.close(); } catch (IOException e) {/* We ignore these. */}
-    _connected = false;
-  }
+		// Set the log level specifying which message levels will be logged by
+		// this logger
+		_logger.setLevel(Level.INFO);
+		ConsoleHandler ch = new ConsoleHandler();
+		ch.setLevel(Level.OFF);
+		_logger.addHandler(ch);
+		/* start the logger */
+		_logger.fine("logger started.");
+	}
 
+	public boolean isClosed() {
+		return _socket.isClosed();
+	}
 
-  /**
-   * Transmit a message to the server
-   */
-  public void transmit( Command c, Map h, String b ) {
-    try {
-      Transmitter.transmit( c, h, b, _output );
-    } catch (Exception e) {
-      receive( Command.ERROR, null, e.getMessage() );
-    }
-  }
+	public void disconnect(Map header) {
+		if (!isConnected())
+			return;
+		transmit(Command.DISCONNECT, header, null);
+		_listener.interrupt();
+		Thread.yield();
+		try {
+			_input.close();
+		} catch (IOException e) {/* We ignore these. */
+		}
+		try {
+			_output.close();
+		} catch (IOException e) {/* We ignore these. */
+		}
+		try {
+			_socket.close();
+		} catch (IOException e) {/* We ignore these. */
+		}
+		_connected = false;
+	}
+
+	/**
+	 * Transmit a message to the server
+	 */
+	public void transmit(Command c, Map h, String b) {
+		try {
+			Transmitter.transmit(c, h, b, _output);
+		} catch (Exception e) {
+			receive(Command.ERROR, null, e.getMessage());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Stomp.Stomp#send(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void send(String dest, String mesg) {
+		_logger.severe("\nMessage sent:\ndestination:" + dest + "\n\n" + mesg
+				+ "\n");
+		super.send(dest, mesg);
+	}
+
 }
---
+++ src/Stomp/OurFormatter.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Stomp;
+
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * @author lxmonk
+ * 
+ */
+public class OurFormatter extends SimpleFormatter {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+	 */
+	@Override
+	public String format(LogRecord record) {
+		return this.formatMessage(record) + "\n";
+	}
+
+}
