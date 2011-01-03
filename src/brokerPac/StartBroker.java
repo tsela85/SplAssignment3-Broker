333a11ee07056532cb5ee2fbd6d1f75086e134fd
Aviad Reich <avi.rei@gmail.com>
2011-01-03 23:11:57 +0200

    build.xml fixed.

--- src/brokerPac/StartBroker.java
+++ src/brokerPac/StartBroker.java
@@ -10,24 +10,28 @@ import javax.security.auth.login.LoginException;
 
 /**
  * @author tom
- *
+ * 
  */
 public class StartBroker {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws LoginException 
-	 * @throws FileNotFoundException 
-	 * @throws NumberFormatException 
+	 * @throws IOException
+	 * @throws LoginException
+	 * @throws FileNotFoundException
+	 * @throws NumberFormatException
 	 */
-	public static void main(String[] args) throws NumberFormatException, FileNotFoundException, LoginException, IOException {
-		Broker broker1=new Broker(args[0],args[1],Integer.parseInt(args[2]));;
+	public static void main(String[] args) throws NumberFormatException,
+			FileNotFoundException, LoginException, IOException {
+		Broker broker1 = new Broker(args[0], args[1], Integer.parseInt(args[2]));
+
 		broker1.connectToStockExcange();
-//		Broker broker2=new Broker("broker2",args[1],Integer.parseInt(args[2]));;
-//		broker2.connectToStockExcange();
-//		Broker broker3=new Broker("broker3",args[1],Integer.parseInt(args[2]));;
-//		broker3.connectToStockExcange();
+		// Broker broker2=new
+		// Broker("broker2",args[1],Integer.parseInt(args[2]));;
+		// broker2.connectToStockExcange();
+		// Broker broker3=new
+		// Broker("broker3",args[1],Integer.parseInt(args[2]));;
+		// broker3.connectToStockExcange();
 	}
 
 }
